f3f3c2a7ba9dbd755f3f8c864b33f917634a6033
EpicGibbon <michant1997@gmail.com>
2020-07-07 19:18:43 -0400

    got the tests running

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 //example of how its supposed to look
 const emptyArr = [];
